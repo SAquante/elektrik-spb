33945aa3c74cd0aea45d05bcf0610bdb35eb0e0d
SAquante <noreply@saquante.local>
2026-09-22 22:10:11 +0300

    Allow CORS for lead API from GitHub Pages

--- server.js
+++ server.js
@@ -198,6 +198,14 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.static(path.join(__dirname, "public"), { extensions: ["html"] }));
 
